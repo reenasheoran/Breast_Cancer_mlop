ddbb387a64d566a940833e2fb66ad416e8ed625a
Reena Sheoran <73627453+reenasheoran@users.noreply.github.com>
2021-05-20 11:20:47 -0600

    created get_data.py

--- source_files/get_data.py
+++ source_files/get_data.py
@@ -12,7 +12,7 @@ def get_data(config_path):
     config =  read_params(config_path)
     data_path = config["data_source"]["gdrive_source"]
     df = pd.read_csv(data_path,sep=',',encoding ='utf-8')
-    print(df.head())
+    return df
 
 
 
@@ -20,4 +20,4 @@ if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config",default='params.yaml')
     args_parsed=args.parse_args()
-    get_data(config_path=args_parsed.config)
+    data = get_data(config_path=args_parsed.config)
